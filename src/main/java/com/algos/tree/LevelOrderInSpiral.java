5c0c44ae32b974b8de2d1245770ac000e40f8a15
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-02 15:47:23 +0530

    guddu

---
+++ src/main/java/com/algos/tree/LevelOrderInSpiral.java
@@ -0,0 +1,59 @@
+package com.algos.tree;
+
+import java.util.*;
+
+
+public class LevelOrderInSpiral {
+    public static void main(String[] args) {
+        MirrorTree.Node root = new MirrorTree.Node(1);
+        root.left = new MirrorTree.Node(2);
+        root.right = new MirrorTree.Node(3);
+        root.left.left = new MirrorTree.Node(4);
+        root.left.right = new MirrorTree.Node(5);
+        root.right.left = new MirrorTree.Node(6);
+        root.right.right = new MirrorTree.Node(7);
+            /*       1
+                2       3
+            4      5  6     7*/
+        levelOrderInSprial(root).forEach(x->System.out.println(x));
+    }
+
+    public static List<List<Integer>> levelOrderInSprial(MirrorTree.Node root) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(root==null)
+            return result;
+
+        Deque<MirrorTree.Node> queue=new ArrayDeque<>();
+        queue.add(root);
+        boolean spiral = true;
+        while(!queue.isEmpty()){
+            int size=queue.size();
+
+            List<Integer> list=new ArrayList<Integer>();
+            if(spiral) {
+                for (int i = 0; i < size; i++) {
+                    MirrorTree.Node node = queue.pollFirst();
+                    list.add(node.data);
+
+                    if (node.left != null)
+                        queue.addLast(node.left);
+                    if (node.right != null)
+                        queue.addLast(node.right);
+                }
+            } else {
+                for (int i = 0; i < size; i++) {
+                    MirrorTree.Node node = queue.pollLast();
+                    list.add(node.data);
+
+                    if (node.left != null)
+                        queue.addFirst(node.right);
+                    if (node.right != null)
+                        queue.addFirst(node.left);
+                }
+            }
+            spiral=!spiral;
+            result.add(list);
+        }
+        return result;
+    }
+}
