6424725e007e51f8548202cf302e3e56f41322f5
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-02 13:57:40 +0530

    guddu

---
+++ src/main/java/com/algos/arrays/WaveArray.java
@@ -0,0 +1,29 @@
+package com.algos.arrays;
+
+import java.util.Arrays;
+
+public class WaveArray {
+
+    public static void main(String[] args) {
+        int [] arr = new int[]{1, 2, 3, 4, 5, 6};
+        createWaveArray(arr);
+        Arrays.stream(arr).forEach((value) -> System.out.print(" " + value));
+    }
+
+    private static void createWaveArray(int[] ints) {
+        for (int i = 0; i < ints.length; i+=2) {
+            if(i>0 && ints[i] < ints[i-1]){
+                swap(ints,i,i-1);
+            }
+            if(i<ints.length-1 && ints[i] < ints[i+1]){
+                swap(ints,i,i+1);
+            }
+        }
+    }
+
+    private static void swap(int[] ints, int i, int i1) {
+        int temp = ints[i];
+        ints[i] = ints[i1];
+        ints[i1] = temp;
+    }
+}
---
+++ src/main/java/com/algos/tree/FindLevelOfMaxOfSumOfNodes.java
@@ -0,0 +1,42 @@
+package com.algos.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindLevelOfMaxOfSumOfNodes {
+
+    public static void main(String[] args) {
+        MirrorTree.Node root = new MirrorTree.Node(1);
+        root.left = new MirrorTree.Node(2);
+        root.right = new MirrorTree.Node(2);
+        root.left.left = new MirrorTree.Node(3);
+        root.left.right = new MirrorTree.Node(4);
+        root.right.left = new MirrorTree.Node(5);
+        root.right.right = new MirrorTree.Node(4);
+        System.out.println(levelOfMaxSumOfNodes(root));
+    }
+
+    private static int levelOfMaxSumOfNodes(MirrorTree.Node root) {
+        int max = 0 ;
+        if (root == null)
+            return max;
+        max = root.data;
+        Queue<MirrorTree.Node> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int sum = 0;
+            int size = queue.size();
+            for (int i = 0; i <size; i++) {
+                MirrorTree.Node node = queue.poll();
+                sum += node.data;;
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
+
+}
