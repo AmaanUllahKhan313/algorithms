cba6b8c4806ab9128312e785eb24f421e9a1efcf
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-06 00:54:25 +0530

    guddu

--- src/main/java/com/algos/tree/ValidateBST.java
+++ src/main/java/com/algos/tree/ValidateBST.java
@@ -2,22 +2,20 @@ package com.algos.tree;
 
 public class ValidateBST {
     public static void main(String[] args) {
-        BTree root = BTree.initializeTree();
-        System.out.println(new ValidateBST().isBST(root));
+        BTree root = new BTree(3);
+        root.left = new BTree(2);
+        root.right = new BTree(5);
+        root.left.left = new BTree(1);
+        root.left.right = new BTree(4);
+        System.out.println(new ValidateBST().isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE));
     }
     BTree prev;
-    public boolean isBST(BTree node)
-    {
-        if (node != null)
-        {
-            if (!isBST(node.left))
-                return false;
-
-            if (prev != null && node.data <= prev.data )
-                return false;
-            prev = node;
-            return isBST(node.right);
-        }
-        return true;
+    public boolean isBST(BTree node,int min, int max){
+        if (node == null)
+            return true;
+        if (node.data < min || node.data > max)
+            return false;
+        return (isBST(node.left, min, node.data-1) &&
+                isBST(node.right, node.data+1, max));
     }
 }
