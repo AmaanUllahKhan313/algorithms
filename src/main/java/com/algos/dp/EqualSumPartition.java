ad6f07ec68353775fbd2a7cd4123adb8c2d969cd
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-10 00:26:24 +0530

    guddu

---
+++ src/main/java/com/algos/dp/EqualSumPartition.java
@@ -0,0 +1,17 @@
+package com.algos.dp;
+
+public class EqualSumPartition {
+    public static void main(String[] args) {
+        System.out.println(getEqualSumPartition(new int []{1,5,11,5}));
+    }
+
+    private static boolean getEqualSumPartition(int[] ints) {
+        int sum = 0;
+        for (int i = 0; i < ints.length; i++) {
+            sum+=ints[i];
+        }
+        return sum%2==0
+                ?FindAllSubsetOfGivenSum.getSubset(ints,sum/2)
+                :false;
+    }
+}
