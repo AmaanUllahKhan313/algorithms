e73211551888f594ac976d4e298ccf9860f13fc1
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-14 00:11:06 +0530

    guddu

--- src/main/java/com/algos/arrays/TripletSum.java
+++ src/main/java/com/algos/arrays/TripletSum.java
@@ -6,14 +6,34 @@ public class TripletSum {
     public static void main(String[] args) {
 //        int[] a = {1, 2, 3, 4, 5};
 //        int sum = 9;
-        int[] a = {12, 3, 4, 1, 6, 9};
-        int sum = 24;
+        int[] a = {12, 3, 4, 1, 7, 9};
+        int sum = 23;
 
-        if (isTripletSumFound(a, sum)) {
+        if (get3Sum(a, sum)) {
             System.out.println("TRIPLET FOUND!");
         } else {
             System.out.println("TRIPLET NOT FOUND");
         }
+    }
+    private static boolean get3Sum(int[] a, int sum) {
+        Arrays.sort(a);
+        int first=0,second=1,last=a.length-1;
+        while (first!=a.length-2){
+            if (sum==a[first]+a[second]+a[last])
+                return true;
+            else
+            if (sum>a[first]+a[second]+a[last])
+                second++;
+            else
+            if (sum<a[first]+a[second]+a[last])
+                last--;
+            if(second==last){
+                first++;
+                second=first+1;
+                last=a.length-1;
+            }
+        }
+        return false;
     }
     private void hashMapImplementation(){
         //TODO - hashmap implementation for triplets sum
