1b9586ca1856035bc9ca693b1a540de19dde57de
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-26 01:38:21 +0530

    guddu

---
+++ src/main/java/com/algos/searching/FindFromRotatedSortedArray.java
@@ -0,0 +1,30 @@
+package com.algos.searching;
+
+public class FindFromRotatedSortedArray {
+    public static void main(String[] args) {
+        System.out.println(search(new int [] { 3,4,5,6,7,8,9,1,2},2));
+    }
+    public static int search(int[] A, int target) {
+        int lo = 0;
+        int hi = A.length - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (A[mid] == target) return mid;
+
+            if (A[lo] <= A[mid]) {
+                if (target >= A[lo] && target < A[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > A[mid] && target <= A[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return A[lo] == target ? lo : -1;
+    }
+}
