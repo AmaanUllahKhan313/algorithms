abfd25f8f42267d1f7c4efe8865fa2da6b7eef6d
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-28 00:54:40 +0530

    guddu

---
+++ src/main/java/com/algos/maths/JumpsRequiredToCrossWall.java
@@ -0,0 +1,22 @@
+package com.algos.maths;
+
+public class JumpsRequiredToCrossWall {
+    public static void main(String[] args) {
+        int height [] = {11, 10, 10, 9};
+        int climb = 10;
+        int slips=1;
+
+        int jumps=0;
+        for (int i = 0; i < height.length; i++) {
+            if (height[i] > climb) {
+                int h = height[i];
+                while (h > climb) {
+                    h = h - (climb - slips);
+                    jumps++;
+                }
+            }
+            jumps++;
+        }
+        System.out.println(jumps);
+    }
+}
