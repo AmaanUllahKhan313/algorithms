5cf8573f31a4c391ac32ed4ecf7e577419689cd4
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-27 19:25:29 +0530

    guddu

---
+++ src/main/java/com/algos/maths/AddNumberWithoutUsingPlusOperator.java
@@ -0,0 +1,13 @@
+package com.algos.maths;
+
+public class AddNumberWithoutUsingPlusOperator {
+    public static void main(String[] args) {
+        int  a = 1, b = 2;
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        System.out.println(a);
+    }
+}
