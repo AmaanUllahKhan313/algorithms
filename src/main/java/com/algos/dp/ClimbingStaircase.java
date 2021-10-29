3f3b20c2c827f02a25a22cf34c172670c33ddabe
Amman Ullah Khan <amaanindia313@gmail.com>
2021-10-29 17:52:53 +0530

    guddu

--- src/main/java/com/algos/dp/ClimbingStaircase.java
+++ src/main/java/com/algos/dp/ClimbingStaircase.java
@@ -3,7 +3,7 @@ package com.algos.dp;
 public class ClimbingStaircase {
     public static void main(String[] args) {
         int stairCase  = 5;
-        System.out.println(countPossibleWaysToClimbStairCase(stairCase));
+        System.out.println(climbStairs(stairCase));
     }
     private static int countPossibleWaysToClimbStairCase(int stairCase) {
         if (stairCase==0)
@@ -14,5 +14,17 @@ public class ClimbingStaircase {
             return 2;
         return countPossibleWaysToClimbStairCase(stairCase-1)
                 + countPossibleWaysToClimbStairCase(stairCase-2);
+    }
+    public static int climbStairs(int n) {
+        if (n <= 1)
+            return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
