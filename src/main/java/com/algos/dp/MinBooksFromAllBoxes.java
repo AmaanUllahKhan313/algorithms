49fe6e17b876791fc97fb3cd98c7502e0bc67792
Amman Ullah Khan <amaanindia313@gmail.com>
2021-12-02 01:29:07 +0530

    guddu

---
+++ src/main/java/com/algos/dp/MinBooksFromAllBoxes.java
@@ -0,0 +1,20 @@
+package com.algos.dp;
+
+public class MinBooksFromAllBoxes {
+    public static void main(String[] args) {
+        int boxes [] = {1,5,3,1,2};
+        System.out.println(getMinBooksFromBoxes(boxes));
+    }
+
+    private static int getMinBooksFromBoxes(int[] boxes) {
+        if (boxes.length==1)
+            return boxes[0];
+        int sum1 = boxes[0];
+        int sum2 = 0;
+        for (int i = 2; i < boxes.length; i+=2) {
+            sum1+=boxes[i];
+            sum2+=boxes[i-1];
+        }
+        return Math.min(sum1,sum2);
+    }
+}
