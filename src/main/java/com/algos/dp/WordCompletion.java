9c29ee5e66e53c145b0d7fc937b85d781207d6a6
Amman Ullah Khan <amaanindia313@gmail.com>
2021-12-02 01:03:12 +0530

    guddu

---
+++ src/main/java/com/algos/dp/WordCompletion.java
@@ -0,0 +1,23 @@
+package com.algos.dp;
+
+public class WordCompletion {
+    public static void main(String[] args) {
+        System.out.println(canComplete("butl","beautiful"));
+    }
+
+    private static boolean canComplete(String str1, String str2) {
+        return str1.length()==LongestCommonSubsequence.getLongestCommonSubsequenceCount(str1,str1.length(),str2,str2.length());
+    }
+
+    public static int getLCSCount(String str1,int m, String str2,int n) {
+        int [] [] dp = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i-1)==str2.charAt(j-1))
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else Math.max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp[m][n];
+    }
+}
