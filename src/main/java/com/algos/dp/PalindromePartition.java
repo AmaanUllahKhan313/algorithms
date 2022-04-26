5b3f627e8abe3d13dcab63c249bf181f8804452c
Amman Ullah Khan <amaanindia313@gmail.com>
2022-04-27 01:36:41 +0530

    guddu

---
+++ src/main/java/com/algos/dp/PalindromePartition.java
@@ -0,0 +1,24 @@
+package com.algos.dp;
+
+public class PalindromePartition {
+    public static void main(String[] args) {
+        String input = "ababbbabbababa";
+        System.out.println(getMinPalindromePartition(input,0,input.length()-1));
+    }
+
+    private static int getMinPalindromePartition(String input, int i, int j) {
+    if(i>=j || isPalindrome(input.substring(i,j+1)))
+        return 0;
+    int result = Integer.MAX_VALUE,count ;
+        for (int k = i; k < j; k++) {
+            count = getMinPalindromePartition(input, i, k)
+                    + getMinPalindromePartition(input, k + 1, j) + 1;
+            result = Math.min(result, count);
+        }
+        return result;
+    }
+
+    private static boolean isPalindrome(String input) {
+        return input.equals(new StringBuffer(input).reverse().toString());
+    }
+}
