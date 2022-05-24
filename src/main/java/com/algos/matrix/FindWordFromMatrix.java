15ededa72207b0ad4bc5b4a3f88755256b25a1d9
apathan <apathan@rocketsoftware.com>
2022-05-24 14:41:42 +0530

    guddu

--- src/main/java/com/algos/matrix/FindWordFromMatrix.java
+++ src/main/java/com/algos/matrix/FindWordFromMatrix.java
@@ -5,9 +5,29 @@ public class FindWordFromMatrix {
         char[][] grid = { { 'G', 'E', 'E', 'K', 'S', 'F', 'O', 'R', 'G', 'E', 'E', 'K', 'S' },
                 { 'G', 'E', 'E', 'K', 'S', 'Q', 'U', 'I', 'Z', 'G', 'E', 'E', 'K' },
                 { 'I', 'D', 'E', 'Q', 'A', 'P', 'R', 'A', 'C', 'T', 'I', 'C', 'E' } };
-        wordSearch(grid, "GEEKS");
-       /* System.out.println();
-        wordSearch(grid, "EEE");*/
+        System.out.println(exist(grid, "GEEKS"));
+    }
+    public static boolean exist(char[][] board, String word) {
+        char[] w = word.toCharArray();
+        for (int y=0; y<board.length; y++) {
+            for (int x=0; x<board[y].length; x++) {
+                if (exist(board, y, x, w, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean exist(char[][] board, int y, int x, char[] word, int i) {
+        if (i == word.length) return true;
+        if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+        if (board[y][x] != word[i]) return false;
+        board[y][x] ^= 256;
+        boolean exist = exist(board, y, x+1, word, i+1)
+                || exist(board, y, x-1, word, i+1)
+                || exist(board, y+1, x, word, i+1)
+                || exist(board, y-1, x, word, i+1);
+        board[y][x] ^= 256;
+        return exist;
     }
     private static void wordSearch(char[][] grid, String word) {
         int[] xDirection = { -1, -1, -1, 0, 0, 1, 1, 1 };
@@ -19,8 +39,8 @@ public class FindWordFromMatrix {
                         int rowMove = row + xDirection[move], columnMove = column + yDirection[move];
                         for (int l = 1; l < word.length(); l++) {
                             if ((rowMove >= 0 && rowMove < grid.length && columnMove >=0 && columnMove < grid[0].length
-                            && grid[rowMove][columnMove] == word.charAt(l)))
-                            rowMove+=xDirection[move];
+                                    && grid[rowMove][columnMove] == word.charAt(l)))
+                                rowMove+=xDirection[move];
                             columnMove+=yDirection[move];
                         }
                         if(move==word.length())
