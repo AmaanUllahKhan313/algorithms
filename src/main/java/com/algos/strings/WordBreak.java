e5308871ba0ca8cb9fdc1099167f1903f8d7ad04
Amman Ullah Khan <amaanindia313@gmail.com>
2022-05-29 00:43:07 +0530

    guddu

--- src/main/java/com/algos/strings/WordBreak.java
+++ src/main/java/com/algos/strings/WordBreak.java
@@ -1,7 +1,6 @@
 package com.algos.strings;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class WordBreak {
     private static Set<String> dictionary = new HashSet<>();
@@ -24,5 +23,28 @@ public class WordBreak {
                 return true;
         }
         return false;
+    }
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return memDFS(s, wordDict, new HashMap<String, List<String>>());
+    }
+
+    private List<String> memDFS(String s, List<String> wordDict, Map<String, List<String>> map) {
+        if (map.containsKey(s)) return map.get(s);
+        List<String> res = new ArrayList<>();
+
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                if (word.length() == s.length()) {
+                    res.add(word);
+                    continue;
+                }
+                List<String> subList = memDFS(s.substring(word.length()), wordDict, map);
+                for (String sub : subList) {
+                    res.add(word + " " + sub);
+                }
+            }
+        }
+        map.put(s, res);
+        return res;
     }
 }
