9a8ddc917ae155ff735317e488252f0af48a9dea
Amman Ullah Khan <amaanindia313@gmail.com>
2022-06-04 00:40:49 +0530

    guddu

--- src/main/java/com/algos/tree/RightPointer.java
+++ src/main/java/com/algos/tree/RightPointer.java
@@ -6,15 +6,7 @@ public class RightPointer {
         public Node left;
         public Node right;
         public Node next;
-
         public Node() {}
-
-        public Node(int _val, Node _left, Node _right, Node _next) {
-            val = _val;
-            left = _left;
-            right = _right;
-            next = _next;
-        }
         public Node(int _val) {
             this.val = _val;
         }
