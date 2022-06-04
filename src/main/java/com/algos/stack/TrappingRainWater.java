c2ccd7316c06d67f5461718689f2cee246bf0bb5
Amman Ullah Khan <amaanindia313@gmail.com>
2022-06-05 00:21:09 +0530

    guddu

--- src/main/java/com/algos/stack/TrappingRainWater.java
+++ src/main/java/com/algos/stack/TrappingRainWater.java
@@ -17,18 +17,15 @@ public class TrappingRainWater {
     public static int getTrappedWaterArea(int[] a) {
         int[] msr = new int[a.length];
         int[] msl = new int[a.length];
-        int[] min = new int[a.length];
         int area = 0;
         msr[0] = a[0];
         msl[a.length - 1] = a[a.length - 1];
         for (int i = 1; i < a.length; i++)
-            if (a[i] < msr[i - 1]) msr[i] = msr[i - 1];
-            else msr[i] = a[i];
+            msr[i] = Math.max(msr[i - 1],a[i]);
         for (int i = a.length - 2; i >= 0; i--)
-            if (a[i] < msl[i + 1]) msl[i] = msl[i + 1];
-            else msl[i] = a[i];
-        for (int i = 0; i < a.length; i++) min[i] = Math.min(msr[i], msl[i]);
-        for (int i = 0; i < a.length; i++) area = area + min[i] - a[i];
+            msl[i] = Math.max(msl[i + 1],a[i]);
+        for (int i = 0; i < a.length; i++)
+            area = area + Math.min(msr[i], msl[i]) - a[i];
         return area;
     }
 }
