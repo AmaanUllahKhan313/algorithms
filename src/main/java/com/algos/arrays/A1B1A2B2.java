fff83ba30b81b38f2baded27ab74a587b0ddbed4
Amman Ullah Khan <amaanindia313@gmail.com>
2022-06-08 17:15:29 +0530

    guddu

--- src/main/java/com/algos/arrays/A1B1A2B2.java
+++ src/main/java/com/algos/arrays/A1B1A2B2.java
@@ -1,33 +1,23 @@
 package com.algos.arrays;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class A1B1A2B2 {
     public static void main(String[] args) {
-        String [] arr = {"A1","A2","A3","A4","B1","B2","B3","B4"};
-        getArrangedArray(arr);
-      Stream.of(arr).forEach(System.out::print);
+        int [] arr = new int [] {1,2,3,4,4,3};
+        IntStream.of(getArrangedArray(arr)).forEach(System.out::print);
     }
-
-    private static void getArrangedArray(String[] arr) {
-        getShuffledArray(arr,0,arr.length-1);
-    }
-
-    private static void getShuffledArray(String[] arr, int start, int end) {
-        if (start > end)
-            return;
-        if (end - start == 1)
-            return;
-        int mid=(start+end)/2;
-        int temp = mid + 1;
-        int mmid = (start + mid) / 2;
-        for (int i = mmid+1; i <= mid; i++) {
-            String temp1 = arr[i];
-            arr[i] = arr[temp];
-            arr[temp++] = temp1;
+    private static int [] getArrangedArray(int [] nums) {
+        int n = nums.length/2;
+        for(int i=0;i<2*n;i=i+2){
+            int temp=nums[i/2+n];
+            int j;
+            for( j=i/2+n;j>i+1;j--){
+                nums[j]=nums[j-1];
+            }
+            nums[j]=temp;
         }
-        getShuffledArray(arr,start,mid);
-        getShuffledArray(arr,mid+1,end);
+        return nums;
     }
-
 }
