ad2b7d791bb0a25d9294158cdb3629aef4c2eff5
Amman Ullah Khan <amaanindia313@gmail.com>
2022-07-12 12:40:09 +0530

    guddu

--- src/main/java/com/algos/arrays/ProdOfAllExceptSelf.java
+++ src/main/java/com/algos/arrays/ProdOfAllExceptSelf.java
@@ -1,24 +1,21 @@
 package com.algos.arrays;
 
+import java.util.Arrays;
+
 public class ProdOfAllExceptSelf {
     public static void main(String[] args) {
         int  [] nums =new int[] {-1,1,0,-3,3};
-        int n = nums.length;
-        int[] res = new int[n];
+        int[] res = new int[nums.length];
         int right=1,left=1;
-        for(int i=0;i<n;i++){
-            res[i]=1;
-        }
-        for (int i=0;i<n;i++) {
+        Arrays.fill(res,1);
+        for (int i=0;i<nums.length;i++) {
             res[i]*=left;
             left*=nums[i];
         }
-        for(int i=n-1;i>=0;i--) {
+        for(int i=nums.length-1;i>=0;i--) {
             res[i]*=right;
             right*=nums[i];
         }
-        for (int n1: res) {
-            System.out.println(n1);
-        }
+        Arrays.stream(res).forEach(System.out::println);
     }
 }
