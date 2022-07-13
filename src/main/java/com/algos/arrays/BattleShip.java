650a6037ec1814f16d547bbd3447b16260d0feeb
Amman Ullah Khan <amaanindia313@gmail.com>
2022-07-13 20:30:16 +0530

    guddu

---
+++ src/main/java/com/algos/arrays/BattleShip.java
@@ -0,0 +1,22 @@
+package com.algos.arrays;
+
+public class BattleShip {
+    public int countBattleships(char[][] board) {
+        int m = board.length;
+        if (m==0) return 0;
+        int n = board[0].length;
+
+        int count=0;
+
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (! (board[i][j] == '.') &&
+                        ! (i > 0 && board[i-1][j] == 'X') &&
+                        ! (j > 0 && board[i][j-1] == 'X'))
+                    count++;
+            }
+        }
+
+        return count;
+    }
+}
