4f7c3446c2357947b98f687344d89c9a36fa0a50
Amman Ullah Khan <amaanindia313@gmail.com>
2022-08-06 00:11:40 +0530

    guddu

--- src/main/java/com/algos/searching/BinarySearch.java
+++ src/main/java/com/algos/searching/BinarySearch.java
@@ -3,22 +3,26 @@ package com.algos.searching;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] a = {1,2,3,4,5,6,7};
-        System.out.println("Found:"+binarySearch(a,2));
+        System.out.println("Found:"+binarySearch(a,2)[1]);
     }
 
-    private static boolean binarySearch(int[] a,int number) {
-        int l =0 , r = a.length-1, mid = (l+r)/2;
-        for(int i = 0; i < a.length;i ++){
-            if(a[mid]==number)return true;
-            else if(a[mid]>number){
-                r = mid;
-                mid = (l + mid)/2;
+    private static int [] binarySearch(int[] nums,int target) {
+        int low = 0, high = nums.length-1;
+        int [] res = {-1,-1};
+        if(nums.length==1) return new int[]{1};
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target) {
+                res[0] = mid;
+                while(nums[res[0]]!=nums[mid]){
+                    mid++;
+                }
+                res[1] = mid;
+                return res;
             }
-            else {
-                l = mid;
-                mid = (mid + r)/2;
-            }
+            else if(nums[mid] > target) high = mid-1;
+            else low = mid+1;
         }
-        return false;
+        return res;
     }
 }
