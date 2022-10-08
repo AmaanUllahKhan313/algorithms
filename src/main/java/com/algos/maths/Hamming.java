3a4897ad02037d1aa4dfc93795f7ec27e6306e2e
Amman Ullah Khan <amaanindia313@gmail.com>
2022-10-09 01:37:34 +0530

    [guddu]

--- src/main/java/com/algos/maths/Hamming.java
+++ src/main/java/com/algos/maths/Hamming.java
@@ -2,18 +2,12 @@ package com.algos.maths;
 
 public class Hamming {
     public static void main(String[] args) {
-        System.out.println(getHamming(4,8));
+        System.out.println(getHamming(680142203,
+                1111953568));
     }
     public static int getHamming(int x, int y){
-        String xBinary =  String.format("%8s", Integer.toBinaryString(x));
-        String yBinary =  String.format("%8s", Integer.toBinaryString(y));
-        int counter = 0;
-        int i = 0;
-        while (i<8){
-            if (xBinary.charAt(i)!=yBinary.charAt(i))
-                counter++;
-            i++;
-        }
-        return counter;
+        int xor = x ^ y, count = 0;
+        for (int i=0;i<32;i++) count += (xor >> i) & 1;
+        return count;
     }
 }
