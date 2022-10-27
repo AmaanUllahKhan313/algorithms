28b3f89b837d4259749740b95dedcbaff0b2ceb9
Amman Ullah Khan <amaanindia313@gmail.com>
2022-10-28 00:14:37 +0530

    [guddu]

--- src/main/java/com/algos/arrays/BinaryGap.java
+++ src/main/java/com/algos/arrays/BinaryGap.java
@@ -5,16 +5,13 @@ public class BinaryGap {
         System.out.println(solution(147));
     }
     public static int solution(int n) {
-        int finalGap = 0;
-        if (n == 1) {
-            return 0;
-        }
+        if (n == 1) return 0;
+        int finalGap = 0; int tempGap = 0;
         char binaryRep[] = Integer.toBinaryString(n).toCharArray();
-        int tempGap=0;
         for (int x = 0; x < binaryRep.length; x++) {
-            if(binaryRep[x]=='0'){
+            if(binaryRep[x]=='0')
                 tempGap++;
-            }else if(binaryRep[x]=='1'){
+            else if(binaryRep[x]=='1'){
                 finalGap=Math.max(tempGap,finalGap);
                 tempGap=0;
             }
