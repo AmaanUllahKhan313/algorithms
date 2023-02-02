f0fd814bbf385fdc703bbdf9a4183d62a49bc7c0
Amman Ullah Khan <amaanindia313@gmail.com>
2023-02-02 14:48:03 +0530

    [guddu]

--- src/main/java/com/algos/arrays/BitonicArray.java
+++ src/main/java/com/algos/arrays/BitonicArray.java
@@ -2,8 +2,8 @@ package com.algos.arrays;
 
 public class BitonicArray {
     public static void main(String[] args) {
-        int arr [] = {1, 2, 3, 4, 4, 3, 1, 0};
-        System.out.println(validBitonicArray(arr));
+        int arr [] = {6, 7, 8, 11, 9, 5, 2, 1};
+        bitonicPointInArray(arr);
     }
     public static boolean validBitonicArray(int[] A) {
         int N = A.length;
@@ -15,5 +15,15 @@ public class BitonicArray {
         while (i+1 < N && A[i] >= A[i+1])
             i++;
         return i == N-1;
+    }
+
+    public static void bitonicPointInArray(int[] A) {
+        int N = A.length;
+        int i = 0;
+        while (i+1 < N && A[i] <= A[i+1])
+            i++;
+        if (i == 0 || i == N-1)
+            System.out.println("No bitonic point");
+        else System.out.println("Bitonic point "+A[i]+" at "+i+"th Location");
     }
 }
