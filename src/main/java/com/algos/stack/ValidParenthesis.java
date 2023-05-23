316a6007c15337e47204254d140a5a740516396c
Amman Ullah Khan <amaanindia313@gmail.com>
2023-05-24 00:38:22 +0530

    [guddu]

---
+++ src/main/java/com/algos/stack/ValidParenthesis.java
@@ -0,0 +1,33 @@
+package com.algos.stack;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean isValidParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty() || !isMatchingPair(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private static boolean isMatchingPair(char open, char close) {
+        return (open == '(' && close == ')') ||
+                (open == '{' && close == '}') ||
+                (open == '[' && close == ']');
+    }
+
+    public static void main(String[] args) {
+        String s1 = "([{}])";
+        String s2 = "([)]";
+
+        System.out.println(isValidParentheses(s1)); // Output: true
+        System.out.println(isValidParentheses(s2)); // Output: false
+    }
+}
