90a6279409c5b86dae674929ad3e62418385f670
Amman Ullah Khan <amaanindia313@gmail.com>
2023-08-29 22:24:07 +0530

    [guddu]

--- src/main/java/com/algos/arrays/LowerSumEqualsHigherSum.java
+++ src/main/java/com/algos/arrays/LowerSumEqualsHigherSum.java
@@ -3,7 +3,7 @@ package com.algos.arrays;
 public class LowerSumEqualsHigherSum {
     public static void main(String[] args) {
         int[] a = {1,2,3,4,2,1,4,5};
-        int partition = findElementGFG(a,a.length);
+        int partition = getPartitionElement(a);
         System.out.println("Elements:");
         for(int element:a) System.out.print(element+"\t");
         System.out.println("Partition element:"+a[partition]);
@@ -26,38 +26,5 @@ public class LowerSumEqualsHigherSum {
             if(prefixSum[i]==suffixSum[i])return i;
         }
         return -1;
-    }
-    //TODO not working
-    static int findElementGFG(int arr[], int size)
-    {
-        int right_sum = 0, left_sum = 0;
-        // Maintains left cumulative sum
-        left_sum = 0;
-
-        // Maintains right cumulative sum
-        right_sum=0;
-        int i = -1, j = -1;
-
-        for( i = 0, j = size-1 ; i < j ; i++, j-- ){
-            left_sum += arr[i];
-            right_sum += arr[j];
-
-            // Keep moving i towards center until
-            // left_sum is found lesser than right_sum
-            while(left_sum < right_sum && i < j){
-                i++;
-                left_sum += arr[i];
-            }
-            // Keep moving j towards center until
-            // right_sum is found lesser than left_sum
-            while(right_sum < left_sum && i < j){
-                j--;
-                right_sum += arr[j];
-            }
-        }
-        if(left_sum == right_sum && i == j)
-            return arr[i];
-        else
-            return -1;
     }
 }
