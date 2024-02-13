ad5f788ca0011849dafe3b68305f7f0735403db9
Amman Ullah Khan <amaanindia313@gmail.com>
2024-02-14 00:55:45 +0530

    [guddu]

--- src/main/java/com/algos/sorting/MergeSort.java
+++ src/main/java/com/algos/sorting/MergeSort.java
@@ -22,15 +22,21 @@ public class MergeSort {
     }
 
     private static void merge(int[] arr, int left, int mid, int right) {
+        // Find sizes of two subarrays to be merged
         int n1 = mid - left + 1;
         int n2 = right - mid;
+        // Create temp arrays
         int L[] = new int[n1];
         int R[] = new int[n2];
+        // Copy data to temp arrays
         for (int i = 0; i < n1; ++i)
             L[i] = arr[left + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[mid + 1 + j];
+        // Merge the temp arrays
+        // Initial indices of first and second subarrays
         int i = 0, j = 0;
+        // Initial index of merged subarray array
         int k = left;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
@@ -43,11 +49,13 @@ public class MergeSort {
             }
             k++;
         }
+        // Copy remaining elements of L[] if any
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
+        // Copy remaining elements of R[] if any
         while (j < n2) {
             arr[k] = R[j];
             j++;
