e2f0f003acaca8f383b52f84ed611d8bb7b1abfb
Amman Ullah Khan <amaanindia313@gmail.com>
2024-02-14 00:30:17 +0530

    [guddu]

---
+++ src/main/java/com/algos/sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package com.algos.sorting;
+
+public class SelectionSort {
+    public static void main(String args[]){
+        System.out.println("Bubble sort program");
+        int[] a ={5,4,3,2,1};
+        //   int[] a ={1,2,3,4,5};
+        for(int i:a) System.out.println(i);
+        sort(a);
+        System.out.println("After sorting");
+        for(int i:a) System.out.println(i);
+    }
+    public static void sort(int[] a) {
+        for(int i = 0;i<a.length;i++) {
+            int min = i;
+            for(int j=i+1;j<a.length;j++) {
+                if(a[min]>a[j])min= j;
+            }
+            int swap = a[i];
+            a[i] = a[min];
+            a[min] = swap;
+        }
+    }
+}
