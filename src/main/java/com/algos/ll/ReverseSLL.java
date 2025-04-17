29cb693a772eb4d4dce89f011a49584294c87359
Amman Ullah Khan <amaanindia313@gmail.com>
2025-04-17 18:05:06 +0530

    [guddu]

--- src/main/java/com/algos/ll/ReverseSLL.java
+++ src/main/java/com/algos/ll/ReverseSLL.java
@@ -1,6 +1,6 @@
 package com.algos.ll;
 
-public class ReverseSLL {
+public class   ReverseSLL {
     public static void main(String[] args) {
         System.out.println("Reversal program");
         NodeSLL head = NodeSLL.getDefaultSLL();
@@ -9,17 +9,19 @@ public class ReverseSLL {
         head.display(head);
     }
 
-    public static NodeSLL reverseSLL(NodeSLL head) {
-        if(head == null) return head;
-        if(head.next == null) return head;
-        NodeSLL ptr1 = null,ptr2 = head,ptr3=head.next;
-        while (ptr2 != null) {
-            ptr2.next = ptr1;
-            ptr1 = ptr2;
-            ptr2 = ptr3;
-            if(ptr3 !=null) ptr3 = ptr3.next;
+    public static NodeSLL reverseSLL(NodeSLL originalHead) {
+        if(originalHead == null) return originalHead;
+        if(originalHead.next == null) return originalHead;
+        NodeSLL previous = null,
+                head = originalHead,
+                headNext = originalHead.next;
+        while (head != null) {
+            head.next = previous;
+            previous = head;
+            head = headNext;
+            if(headNext !=null) headNext = headNext.next;
         }
-        head = ptr1;
-        return head;
+        originalHead = previous;
+        return originalHead;
     }
 }
