f88e493dba1f41853671c288fc86764815ffdf8e
Amman Ullah Khan <amaanindia313@gmail.com>
2025-04-19 01:50:49 +0530

    [guddu]

--- src/main/java/com/algos/ll/Intersection.java
+++ src/main/java/com/algos/ll/Intersection.java
@@ -49,4 +49,16 @@ public class Intersection {
         }
             return -1;
     }
+
+    public ListNode getIntersectionNodeEasy(ListNode headA, ListNode headB) {
+        ListNode h1 = headA;
+        ListNode h2 = headB;
+
+        while (h1 != h2) {
+            h1 = h1 == null ? headB : h1.next;
+            h2 = h2 == null ? headA : h2.next;
+        }
+
+        return h1;
+    }
 }
