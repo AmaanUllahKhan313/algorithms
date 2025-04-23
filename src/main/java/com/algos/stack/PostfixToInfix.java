52cad3d311272b8350dcfef8698be9d8593ce8c6
Amman Ullah Khan <amaanindia313@gmail.com>
2025-04-24 00:33:00 +0530

    [guddu]

--- src/main/java/com/algos/stack/PostfixToInfix.java
+++ src/main/java/com/algos/stack/PostfixToInfix.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class PostfixToInfix {
     public static void main(String[] args) {
-        System.out.println(postFixToInfix("ab*c+"));
+        System.out.println(new PostfixToInfix().evalRPN(("4 13 5 / +").split(" ")));
     }
 
     private static String postFixToInfix(String postfix) {
@@ -18,8 +18,39 @@ public class PostfixToInfix {
         }
         return stack.pop();
     }
+    public int evalRPN(String[] postfix) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < postfix.length; i++) {
+            if (isOperand(postfix[i])){
+                stack.push(Integer.parseInt(postfix[i]));
+            } else {
+                int secondNumber =  stack.pop();
+                int firstNumber  =  stack.pop();
+                stack.push(performOperation(firstNumber,secondNumber, postfix[i]));
+            }
+        }
+        return stack.pop();
+    }
 
     private static boolean isOperand(char charAt) {
         return charAt>='a'&&charAt<='z'||charAt>='A'&&charAt<='Z';
     }
+    private static boolean isOperand(String charAt) {
+        return charAt.matches("-?\\d+(\\.\\d+)?");
+    }
+    public static int performOperation(int number1, int number2, String operator){
+        if (operator.equals("+")){
+            return number1+number2;
+        }
+        else if (operator.equals("-")){
+            return number1-number2;
+        }
+        else if (operator.equals("*")){
+            return number1*number2;
+        }
+        else{
+            return number1/number2;
+        }
+    }
 }
+
