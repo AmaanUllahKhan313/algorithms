a6679dd5f5a995bbaa6d516d61b651cd7118add4
Amman Ullah Khan <amaanindia313@gmail.com>
2025-05-04 16:18:03 +0530

    [guddu]

--- src/main/java/com/algos5_queue/_1QueueUsingArray.java
+++ src/main/java/com/algos5_queue/_1QueueUsingArray.java
@@ -1,16 +1,16 @@
 package com.algos5_queue;
-class QueueUsingArray {
+class _1QueueUsingArray {
     int front, rear, size;
     int capacity;
     int array[];
-    public QueueUsingArray(int capacity) {
+    public _1QueueUsingArray(int capacity) {
         this.capacity = capacity;
         front = this.size = 0;
         rear = capacity - 1;
         array = new int[this.capacity];
     }
     public static void main(String[] args){
-        QueueUsingArray queue = new QueueUsingArray(1000);
+        _1QueueUsingArray queue = new _1QueueUsingArray(1000);
         queue.enqueue(10);
         queue.enqueue(20);
         queue.enqueue(30);
