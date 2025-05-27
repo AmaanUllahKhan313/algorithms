27cdbf539563dac1c5b4cdd69124c60fe2ade3e3
Amman Ullah Khan <amaanindia313@gmail.com>
2025-05-27 22:50:40 +0530

    [guddu]

--- src/main/java/com/algos10_tree/_5PrintTreeLevelOrder.java
+++ src/main/java/com/algos10_tree/_5PrintTreeLevelOrder.java
@@ -8,28 +8,21 @@ import java.util.Queue;
 
 public class _5PrintTreeLevelOrder {
     public static void main(String[] args) {
-        _17IsMirrorTree.Node root = new _17IsMirrorTree.Node(1);
-        root.left = new _17IsMirrorTree.Node(2);
-        root.right = new _17IsMirrorTree.Node(2);
-        root.left.left = new _17IsMirrorTree.Node(3);
-        root.left.right = new _17IsMirrorTree.Node(4);
-        root.right.left = new _17IsMirrorTree.Node(5);
-        root.right.right = new _17IsMirrorTree.Node(4);
-        System.out.println(levelOrder(root));
+        System.out.println(levelOrder(_1BTree.initializeTree()));
     }
-    public static List<List<Integer>> levelOrder(_17IsMirrorTree.Node root) {
+    public static List<List<Integer>> levelOrder(_1BTree root) {
         List<List<Integer>> result=new ArrayList<>();
         if(root==null)
             return result;
 
-        Queue<_17IsMirrorTree.Node> queue=new LinkedList<>();
+        Queue<_1BTree> queue=new LinkedList<>();
         queue.add(root);
         while(!queue.isEmpty()){
             int size=queue.size();
 
             List<Integer> list=new ArrayList<Integer>();
             for(int i=0;i<size;i++){
-                _17IsMirrorTree.Node node=queue.poll();
+                _1BTree node=queue.poll();
                 list.add(node.data);
 
                 if(node.left!=null)
