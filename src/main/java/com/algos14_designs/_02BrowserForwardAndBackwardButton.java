35664a829db7a889244f81d21cc9427638242cce
Amman Ullah Khan <amaanindia313@gmail.com>
2025-08-19 21:34:36 +0530

    [guddu]

--- src/main/java/com/algos14_designs/_02BrowserForwardAndBackwardButton.java
+++ src/main/java/com/algos14_designs/_02BrowserForwardAndBackwardButton.java
@@ -1,74 +1,74 @@
 package com.algos14_designs;
 
-public class _02BrowserForwardAndBackwardButton {
-    static Urls urls = null;
-    static class Urls {
-        private String value;
-        private Urls next;
-        private Urls previous;
-        public String getValue() {
-            return value;
-        }
-        public void setValue(String value) {
-            this.value = value;
-        }
-        public Urls getNext() {
-            return next;
-        }
-        public void setNext(Urls next) {
-            this.next = next;
-        }
-        public Urls getPrevious() {
-            return previous;
-        }
-        public void setPrevious(Urls previous) {
-            this.previous = previous;
-        }
-    }
-    public static Urls addingAndNewUrl(String string){
-        if(null==urls) {
-            urls = new Urls();
-            urls.setValue(string);
-            urls.setPrevious(null);
-            urls.setNext(null);
-        }
-        if(urls.getNext()!=null){
-            urls.setNext(null);
-            urls.setValue(string);
-        }
-        return urls;
-    }
-    public static Urls onClickBackword(){
-        if(null==urls.getPrevious())
-            return null;
-        else{
-            urls.setValue(urls.getPrevious().getValue());
-            urls.setNext(urls.getPrevious().getNext());
-            urls.setPrevious(urls.getPrevious().getPrevious());
-        }
-        return urls;
-    }
-    public static Urls  onClickforward() {
-        if(null==urls.getNext())
-            return null;
-        else{
-            urls.setValue(urls.getNext().getValue());
-            urls.setNext(urls.getNext().getNext());
-            urls.setPrevious(urls.getNext().getPrevious());
-        }
-        return urls;
-    }
+import java.util.Stack;
 
-    public static void main(String[] args) {
-        String input = "Google.com";
-        System.out.println(addingAndNewUrl(input).getValue());
-        //System.out.println(onClickBackword().getValue());
-        System.out.println(addingAndNewUrl("yahoo").getValue());
-        System.out.println(addingAndNewUrl("amazon").getValue());
-        System.out.println(addingAndNewUrl("flipkart").getValue());
-        System.out.println(onClickBackword().getValue());
-        System.out.println(addingAndNewUrl("microsoft").getValue());
-        System.out.println(onClickBackword().getValue());
-        System.out.println(onClickforward().getValue());
+public class _02BrowserForwardAndBackwardButton {
+    static String current_state_url = "";
+    static Stack<String>
+            forward_stack = new Stack<>();
+    static Stack<String>
+            backward_stack = new Stack<>();
+    static void visit_new_url(String url){
+        if (current_state_url != ""){
+            backward_stack.add(
+                    current_state_url);
+        }
+        current_state_url = url;
+    }
+    static void forward(){
+        if (forward_stack.isEmpty() ||
+                current_state_url ==
+                        forward_stack.peek()){
+            return;
+        }else{
+            backward_stack.add(current_state_url);
+            current_state_url = forward_stack.peek();
+            forward_stack.pop();
+        }
+    }
+    static void backward(){
+        if (backward_stack.isEmpty() ||
+                current_state_url ==
+                        backward_stack.peek()){
+        } else {
+            forward_stack.add(
+                    current_state_url);
+            current_state_url =
+                    backward_stack.peek();
+            backward_stack.pop();
+        }
+    }
+    public static void main(String[] args){
+        String url = "google.com";
+        visit_new_url(url);
+        System.out.print("Current URL is: " +
+                current_state_url +
+                " \n");
+        url = "yahoo.com";
+        visit_new_url(url);
+        System.out.print("Current URL is: " +
+                current_state_url +
+                " \n");
+        backward();
+        System.out.print("Current URL after pressing" +
+                " Backward button is: " +
+                current_state_url + " \n");
+        forward();
+        System.out.print("Current URL after pressing" +
+                " Forward button is: " +
+                current_state_url + " \n");
+        url = "amazon.com";
+        visit_new_url(url);
+        System.out.print("Current URL is: " +
+                current_state_url +
+                " \n");
+        forward();
+        System.out.print("Current URL after pressing" +
+                " Forward button is: " +
+                current_state_url + " \n");
+        backward();
+        System.out.print("Current URL after pressing" +
+                " Backward button is: " +
+                current_state_url + " \n");
     }
 }
