b7b75e63f4739097d64aeecf9c1e0ad82a913ea1
Amman Ullah Khan <amaanindia313@gmail.com>
2025-08-19 21:50:41 +0530

    [guddu]

--- src/main/java/com/algos14_designs/_03MIssingNumberFromSecondArray.java
+++ src/main/java/com/algos14_designs/_03MIssingNumberFromSecondArray.java
@@ -2,16 +2,17 @@ package com.algos14_designs;
 
 public class _03MIssingNumberFromSecondArray {
     public static void main(String[] args) {
-        System.out.println(getMissingNumberFromSecondArray(new int []{4, 8, 1, 3, 7}, new int[]{7, 4, 3, 1}));
+        System.out.println(getMissingNumberFromSecondArray(new
+                int []{4, 8, 1, 3, 7}, new int[]{7, 4, 3, 1}));
     }
 
-    private static int getMissingNumberFromSecondArray(int[] ints, int[] ints1) {
+    private static int getMissingNumberFromSecondArray(int[] firstArray, int[] secondArray) {
         int missingNumber = 0;
-        for (int i = 0; i < ints.length; i++) {
-            missingNumber=missingNumber^ints[i];
+        for (int i = 0; i < firstArray.length; i++) {
+            missingNumber=missingNumber^firstArray[i];
         }
-        for (int i = 0; i < ints1.length; i++) {
-            missingNumber=missingNumber^ints1[i];
+        for (int i = 0; i < secondArray.length; i++) {
+            missingNumber=missingNumber^secondArray[i];
         }
         return missingNumber;
     }
