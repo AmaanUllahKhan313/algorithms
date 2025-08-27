38f18eff237fd805ec380d7b2e9c8dcedeca071a
Amman Ullah Khan <amaanindia313@gmail.com>
2025-08-27 17:58:06 +0530

    [guddu]

--- src/main/java/com/algos15_dynamicprogramming/_07EditDistanceToMatchStrings.java
+++ src/main/java/com/algos15_dynamicprogramming/_07EditDistanceToMatchStrings.java
@@ -13,9 +13,7 @@ public class _07EditDistanceToMatchStrings {
         if(str1.charAt(m-1)==str2.charAt(n-1))
             return getEditDistanceCount(str1,str2,m-1,n-1);
         return 1+Math.min(Math.min(getEditDistanceCount(str1,str2,m,n-1)
-                , getEditDistanceCount(str1,str2,m-1,n))
+                        , getEditDistanceCount(str1,str2,m-1,n))
                 ,getEditDistanceCount(str1,str2,m-1,n-1));
-
-
     }
 }
