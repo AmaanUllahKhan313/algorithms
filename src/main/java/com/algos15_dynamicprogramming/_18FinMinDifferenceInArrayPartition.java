6744e9ecabe82a5a86d510edc351979412c06e6f
Amman Ullah Khan <amaanindia313@gmail.com>
2025-11-29 23:00:20 +0530

    [guddu]

--- src/main/java/com/algos15_dynamicprogramming/_18FinMinDifferenceInArrayPartition.java
+++ src/main/java/com/algos15_dynamicprogramming/_18FinMinDifferenceInArrayPartition.java
@@ -9,33 +9,21 @@ public class _18FinMinDifferenceInArrayPartition {
     public static void main(String[] args) {
         int [] a = {5,12,0,1};
         int sum = 0;
-        for (int i = 0; i < a.length; i++) {
-            sum+=a[i];
-        }
-        int maxDiff= sum/2;
-        boolean [][] t = getSubsetMatrix(a,sum);
-        for (int k = maxDiff; k >= 0; k--)
-        {
-            if (t[a.length][k] == true)
-            {
-                System.out.println(sum - k - k);
+        System.out.println(findMinimumDifferenceInArrayPartition(a, sum));
+    }
+
+    private static int findMinimumDifferenceInArrayPartition(int[] a, int sum) {
+        for (int i = 0; i < a.length; i++)
+            sum += a[i];
+        int maxDiff = sum /2;
+        int minDiff = 0;
+        boolean [][] t = getSubsetMatrix(a, sum);
+        for (int k = maxDiff; k >= 0; k--) {
+            if (t[a.length][k] == true) {
+                minDiff = sum - k - k;
                 break;
             }
         }
-
+        return minDiff;
     }
- /*   1 2 3 4 5 6 7 8 9     14
-   4
-   2
-   8       t f t t
-
-
-            s1-s2 =dif;
-            s1+s2 = total;
-            s1 = diff-total/2 =
-
-14 - 8 - 6 = 2
-    total - 2 * 3 = diff
-    14 -2 *6 = 2*/
-
 }
