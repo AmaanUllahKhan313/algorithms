c1349aaceafd17c00a2520af4336d3c0b2474535
Amman Ullah Khan <amaanindia313@gmail.com>
2025-12-04 17:02:08 +0530

    [guddu]

--- src/main/java/com/algos16_graphs/_03PathExist.java
+++ src/main/java/com/algos16_graphs/_03PathExist.java
@@ -4,21 +4,12 @@ import java.util.*;
 
 public class _03PathExist {
     public static void main(String[] args) {
-        // Create graph
         Map<Integer, List<Integer>> graph = _0Graph.getGraphAsMap();
         int start = 0;
         int end = 3;
         boolean[] visited = new boolean[graph.size()];
-        dfs(graph, visited, start, end);
-        if (found)
-            System.out.println("Path exists between " + start + " and " + end);
-        else
-            System.out.println("No path exists between " + start + " and " + end);
+        System.out.println("Path exists between " + start + " and " + end + " is " + dfs(graph, visited, start, end) );
     }
-
-
-
-    static boolean found = false;
     static boolean dfs(Map<Integer, List<Integer>> graph, boolean[] visited, int start, int end) {
         if (visited[start]) return false;
         if (start == end) return true;
