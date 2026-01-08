08f4182b4f9aece6524c152d27e35905b6b94c3b
Amman Ullah Khan <amaanindia313@gmail.com>
2026-01-09 00:19:46 +0530

    [guddu]

--- src/main/java/com/algos16_graphs/_04NumberOfProvinces.java
+++ src/main/java/com/algos16_graphs/_04NumberOfProvinces.java
@@ -1,24 +1,22 @@
 package com.algos16_graphs;
 
-import java.util.Iterator;
-
 public class _04NumberOfProvinces {
     public static void main(String[] args) {
         System.out.println(getNumberOfProvinces(_0Graph.getGraphAsMatrix()));
     }
-    private static int getNumberOfProvinces(int[][] M) {
+    private static int getNumberOfProvinces(int[][] graph) {
         int count=0;
-        for (int i = 0; i < M.length; i++)
-            if (M[i][i] != 0) {
-                dfs(M, i);
+        for (int i = 0; i < graph.length; i++)
+            if (graph[i][i] != 0) {
+                dfs(graph, i);
                 count++;
             }
         return count;
     }
-    static void dfs(int[][] M, int u) {
-        M[u][u] = 0;
-        for (int v = 0; v < M[u].length; v++)
-            if (M[v][v] != 0 && M[u][v] == 1)
-                dfs(M, v);
+    static void dfs(int[][] graph, int i) {
+        graph[i][i] = 0;
+        for (int j = 0; j < graph[i].length; j++)
+            if (graph[j][j] != 0 && graph[i][j] == 1)
+                dfs(graph, j);
     }
 }
