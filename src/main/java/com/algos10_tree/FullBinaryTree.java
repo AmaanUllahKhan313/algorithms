e3c7ced1d905b3658115065efbcb945fd0f9b62b
Amaan <amaanindia313@gmail.com>
2026-07-27 23:37:09 +0530

    guddu

--- src/main/java/com/algos10_tree/FullBinaryTree.java
+++ src/main/java/com/algos10_tree/FullBinaryTree.java
@@ -1,4 +1,28 @@
 package com.algos10_tree;
 
 public class FullBinaryTree {
+    public static boolean isFullBinaryTree(_1BTree root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left == null && root.right == null) {
+            return true;
+        }
+        if (root.left != null && root.right != null) {
+            return isFullBinaryTree(root.left) &&
+                    isFullBinaryTree(root.right);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        _1BTree root = new _1BTree(1);
+        root.left = new _1BTree(2);
+        root.right = new _1BTree(3);
+        root.left.left = new _1BTree(4);
+        root.left.right = new _1BTree(5);
+        root.right.left = new _1BTree(6);
+        root.right.right = new _1BTree(7);
+        System.out.println(isFullBinaryTree(root)); // true
+    }
 }
