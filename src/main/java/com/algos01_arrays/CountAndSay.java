038393724fae15e793605dec14657a39f625f32c
Amaan <amaanindia313@gmail.com>
2026-09-08 23:25:08 +0530

    guddu

--- src/main/java/com/algos01_arrays/CountAndSay.java
+++ src/main/java/com/algos01_arrays/CountAndSay.java
@@ -13,23 +13,19 @@ public class CountAndSay {
     }
 
     public static String countIdx(String s){
-        StringBuilder sb = new StringBuilder();
+        String sb = new String();
         char c = s.charAt(0);
         int count = 1;
         for(int i = 1; i < s.length(); i++){
-            if(s.charAt(i) == c){
-                count++;
-            }
-            else
-            {
-                sb.append(count);
-                sb.append(c);
+            if(s.charAt(i) == c) count++;
+            else {
+                sb+=count+""+c;
                 c = s.charAt(i);
                 count = 1;
             }
         }
-        sb.append(count);
-        sb.append(c);
-        return sb.toString();
+        sb+=count;
+        sb+=c;
+        return sb;
     }
 }
